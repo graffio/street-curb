456b18e97b378e3cd98bf7dae24b5ce05691424e
Jeff <jeff@range.io>
2025-09-13 16:12:16 -0700

    [refr] update editor types and package dependencies
    - Update blockface.js type definitions
    - Update types/index.js exports
    - Update package.json dependencies
    - Integrate with new shared type system

--- modules/curb-map/editor/src/types/index.js
+++ modules/curb-map/editor/src/types/index.js
@@ -1,5 +1,7 @@
 // Auto-generated module index
 // This file exports all generated types for this module
 
+export { AuditRecord } from './audit-record.js'
 export { Blockface } from './blockface.js'
+export { OperationDetails } from './operation-details.js'
 export { Segment } from './segment.js'
