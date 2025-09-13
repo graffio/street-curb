7db742a0f31c14135a6da7e7d4f7c4e143db5b48
Jeff <jeff@range.io>
2025-09-13 16:12:41 -0700

    [chre] add development configuration files
    - Add modules/curb-map/shared/configs/ directory
    - Add dev.2025-09-11-130012.config.js configuration
    - Add development infrastructure for testing
    - Support config-as-output functionality

---
+++ modules/curb-map/shared/configs/dev.config.js
@@ -0,0 +1,6 @@
+import organization from '../config/organization.js'
+
+export default {
+    ...organization,
+    firebaseProject: { projectId: 'graffio-dev-2025-09-10', displayName: 'Graffio Development', region: 'us-central1' },
+}
