ea4140f64b187afa22aa5e8bf9a608ac0dd28a45
Jeff <jeff@range.io>
2025-09-16 14:42:37 -0700

    003-configure-authentication.js
    + Rolling projects forward because of new migrations

--- modules/curb-map/shared/config/temporary-20250915-164532.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    organizationId: '404973578720',
-    developmentFolderId: '464059598701',
-    billingAccountId: '0127B8-824540-F55374',
-    firebaseProject: { projectId: 'temporary-20250915-164532', displayName: 'temporary-20250915-164532' },
-    firebaseProjectNumber: '151711049596',
-}
