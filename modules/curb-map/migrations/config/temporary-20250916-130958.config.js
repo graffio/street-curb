816ebd7fb10d3c066d271e61ddd922e6e23a7d06
Jeff <jeff@range.io>
2025-09-22 14:14:22 -0700

    Declare firebase-admin dependency     - Add `firebase-admin` to the module dependencies so the integration test’s require resolves (`modules/curb-map/package.json:40-52`).     - Lockfile reflects the new package (`yarn.lock`).
    Update the temporary config to test changes and get rid of old one

--- modules/curb-map/migrations/config/temporary-20250916-130958.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    organizationId: '404973578720',
-    developmentFolderId: '464059598701',
-    billingAccountId: '0127B8-824540-F55374',
-    firebaseProject: { projectId: 'temporary-20250916-130958', displayName: 'temporary-20250916-130958' },
-    firebaseProjectNumber: '592724553361',
-}
